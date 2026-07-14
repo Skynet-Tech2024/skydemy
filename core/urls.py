d5abd7f9c75c31be275b301fd51efebc7a10ced1
Skynet-Tech2024 <nde.kenneth61@gmail.com>
2026-07-14 21:54:21 +0100

    Fix syntax in urls.py

--- core/urls.py
+++ core/urls.py
@@ -5,14 +5,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('captcha/', include('captcha.urls')),  # <-- Add this line
-path('sw.js', service_worker),    
-path('admin/', admin.site.urls),
+    path('captcha/', include('captcha.urls')),
+    path('sw.js', service_worker),
+    path('admin/', admin.site.urls),
     path('', include('dashboard.urls')),
     path('users/', include('users.urls')),
     path('courses/', include('courses.urls')),
 ]
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

fed837025757378bff14083c6911c21b08100edd
Skynet-Tech2024 <nde.kenneth61@gmail.com>
2026-07-14 22:42:55 +0100

    Use static root as static folder and simpler whitenoise storage

--- core/settings.py
+++ core/settings.py
@@ -89,9 +89,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 

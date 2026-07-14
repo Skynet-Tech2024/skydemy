82c75c19a1dbb7236fe32657dfca756bf36a7032
Skynet-Tech2024 <nde.kenneth61@gmail.com>
2026-07-14 21:43:14 +0100

    Add service worker for PWA

---
+++ static/sw.js
@@ -0,0 +1,35 @@
+// Service Worker for SKYDEMY PWA
+const CACHE_NAME = 'skydemy-v1';
+const urlsToCache = [
+    '/',
+    '/static/images/logo.png',
+    '/static/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((name) => {
+                    if (name !== CACHE_NAME) {
+                        return caches.delete(name);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});

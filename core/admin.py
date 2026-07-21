ac2ff66ca00967979f12401a868bbac6eb02d401
Skynet-Tech2024 <nde.kenneth61@gmail.com>
2026-07-21 13:12:00 +0100

    Final attempt: custom admin site with hardcoded footer templates

--- core/admin.py
+++ core/admin.py
@@ -1,17 +1,52 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
+from django.urls import path
 from django.contrib.auth import logout as auth_logout
 from django.shortcuts import redirect
 from django.contrib import messages
-from django.urls import path
+from django.template.response import TemplateResponse
+from django.contrib.admin.views.main import ChangeList
 
-# We'll keep the custom logout view but keep the default admin site
-admin.site.site_header = "SKYDEMY Admin"
-admin.site.site_title = "SKYDEMY Admin"
-admin.site.index_title = "Dashboard"
+class SKYDEMYAdminSite(AdminSite):
+    site_header = "SKYDEMY Admin"
+    site_title = "SKYDEMY Admin"
+    index_title = "Dashboard"
 
-def admin_logout(request):
-    auth_logout(request)
-    messages.info(request, "You have been logged out.")
-    return redirect('/admin/login/')
+    def logout(self, request, extra_context=None):
+        auth_logout(request)
+        messages.info(request, "You have been logged out.")
+        return redirect('/admin/login/')
 
-# Add custom logout URL manually in urls.py
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path('logout/', self.logout, name='admin_logout'),
+        ]
+        return custom_urls + urls
+
+    def index(self, request, extra_context=None):
+        """
+        Override the admin index view to use a custom template with footer.
+        """
+        context = self.each_context(request)
+        context.update(extra_context or {})
+        # Add the footer to the context? No, we'll hardcode it in the template.
+        return TemplateResponse(
+            request,
+            'admin/custom_index.html',  # We'll create this template
+            context
+        )
+
+    def changelist_view(self, request, model_admin, model, **kwargs):
+        """
+        Override the changelist view to use a custom template with footer.
+        """
+        # This is more complex; we can handle it by setting a custom template
+        # on each ModelAdmin, but for simplicity we'll override the response.
+        # We'll instead set a custom template for each model admin that we want.
+        # But for now, we'll just override the index and let the change list
+        # inherit from the custom base.html.
+        return super().changelist_view(request, model_admin, model, **kwargs)
+
+# Replace the default admin site with our custom one
+admin_site = SKYDEMYAdminSite()
--- core/urls.py
+++ core/urls.py
@@ -1,15 +1,16 @@
 from . import views as core_views
 from dashboard.views import service_worker
-from django.contrib import admin  # Use the default admin site
+from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .admin import admin_site  # Import the custom admin site
 
 urlpatterns = [
     path('sw.js', service_worker),
     path('captcha/', include('captcha.urls')),
     path('admin/logout/', core_views.admin_logout, name='admin_logout'),
-    path('admin/', admin.site.urls),  # Now uses default admin
+    path('admin/', admin_site.urls),  # Use the custom admin site
     path('', include('dashboard.urls')),
     path('users/', include('users.urls')),
     path('courses/', include('courses.urls')),

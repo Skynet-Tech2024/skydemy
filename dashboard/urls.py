c3d161c6cb2717711943f7fccf5f1d94a6c6c30c
Skynet-Tech2024 <nde.kenneth61@gmail.com>
2026-07-21 14:26:16 +0100

    Add debug URL to dashboard app

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+path('debug/', views.debug_templates, name='debug_templates'),
     path('', views.home, name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('profile/', views.profile, name='profile'),

83e73f8062b3d0441287753ebca170a2592c044e
Skynet-Tech2024 <nde.kenneth61@gmail.com>
2026-07-22 20:22:04 +0100

    Add placeholder sw.js to fix TemplateDoesNotExist error

---
+++ templates/sw.js
@@ -0,0 +1,10 @@
+// Service Worker placeholder
+self.addEventListener('install', function(e) {
+  e.waitUntil(self.skipWaiting());
+});
+self.addEventListener('activate', function(e) {
+  e.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', function(e) {
+  e.respondWith(fetch(e.request));
+});

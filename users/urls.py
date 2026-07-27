33c59f451bb7fad0cd5a1d923ec7f04f58bc0004
Skynet-Tech2024 <nde.kenneth61@gmail.com>
2026-07-27 08:13:53 +0100

    Add registration success and pending approval URLs

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import register, custom_login, custom_logout, complete_profile
 
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.custom_login, name='login'),
     path('logout/', views.custom_logout, name='logout'),
-path('complete-profile/', complete_profile, name='complete_profile'),
+    path('complete-profile/', views.complete_profile, name='complete_profile'),
+    path('registration-success/', views.registration_success, name='registration_success'),
+    path('pending-approval/', views.pending_approval, name='pending_approval'),
 ]
